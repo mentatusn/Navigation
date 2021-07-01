2282b39467d3630fa009aae2b0d2e1eef24e93d9
Andej Nesterenko <mentatus.n@gmail.com>
2021-07-01 21:29:01 +0300

    Initial commit

---
+++ app/src/main/java/keyone/keytwo/navigation/Settings.java
@@ -0,0 +1,16 @@
+package keyone.keytwo.navigation;
+
+public class Settings {
+    public static final String SHARED_PREFERENCE_NAME = "FragmentNavigation";
+    public static final String IS_BACK_STACK_USED = "UseBackStack";
+    public static final String IS_ADD_FRAGMENT_USED = "UseAddFragment";
+    public static final String IS_REPLACE_FRAGMENT_USED = "UseAddFragment2";
+    public static final String IS_BACK_AS_REMOVE_FRAGMENT = "BackAsRemove";
+    public static final String IS_DELETE_FRAGMENT_BEFORE_ADD = "DeleteFragmentBeforeAdd";
+
+    public static boolean isBackStack;
+    public static boolean isAddFragment;
+    public static boolean isReplaceFragment;
+    public static boolean isBackAsRemove;
+    public static boolean isDeleteBeforeAdd;
+}
